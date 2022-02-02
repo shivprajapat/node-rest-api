744f9bedbb1f2ff0dc4ca41c1d9c1d0f25b33cd1
shivprajapat <shivkumarprajapat232@gmail.com>
2022-02-02 19:50:21 +0530

    post request added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const res = require('express/lib/response');
 
 const PORT = 5000;
 const app = express();
-// app.use(express());
+
+app.use(express.json());
 
 let todos = [
     { id: 1, text: "this is first todo" },
@@ -25,13 +27,37 @@ app.get('/todo/:id', (req, res) => {
 
     if (!todo) {
         res.json({
-            message:"Invalid ID"
+            message: "Invalid ID"
         })
-        
+
     }
     res.json(todo)
+})
+
+// POST
+
+app.post('/todo', (req, res) => {
+    const { text } = req.body;
+
+    const newTodo = { id: todos.length + 1, text };
+    todos.push(newTodo);
+
+    res.json(newTodo);
+
+    // message: "todo added"
 })
 
 app.listen(PORT, () => {
     console.log(`server is runing on http://localhost:${PORT}`);
 })
+
+
+/**
+* API Requset
+* ======
+* GET => show data
+* POST => add data
+* PATCH => id change
+* PUT => all data edit
+* DELETE => delete data
+**/ 

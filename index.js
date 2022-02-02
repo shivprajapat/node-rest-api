0ebaffa753587f371bbcd707346002b96d47ce88
shivprajapat <shivkumarprajapat232@gmail.com>
2022-02-02 09:40:33 +0530

    show all todos list data

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
 const PORT = 5000;
-const app =express();
+const app = express();
+let todos = [
+    { id: 1, text: "this is first todo" },
+    { id: 2, text: "this is second todo" },
+    { id: 3, text: "this is third todo" },
+];
 
-app.get('/',(req,res )=>{
+app.get('/', (req, res) => {
     res.json({ message: "Hello World" });
 })
-app.listen(PORT,()=>{
+
+app.get('/todo', (req, res) => {
+    res.json(todos);
+})
+
+app.listen(PORT, () => {
     console.log(`server is runing on http://localhost:${PORT}`);
 })
